108a3cca0ac467365db7d032dc0497f1da68cd54
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-09-12 18:41:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title,price,quantity=0)
+      if quantity > 0
+        @total += (price*quantity)
+      else
+        @total += price
+      end
+
+      if @discount > 0
+        apply_discount
+        binding.pry
+      end
+      @total
+  end
+
+  def apply_discount
+    @total = @total-(@total*(@discount/100.0))
+  end
+
+end
